0c92b6da141901545a5ff472c633ca89922aa21c
jians123 <100296457+jians123@users.noreply.github.com>
2022-04-04 03:05:24 +0800

    Update page5.js

--- page5.js
+++ page5.js
@@ -9,13 +9,13 @@ let alpha_right = 0;
 let sound;
 
 function preload(){
-    bgImage = loadImage("/image/main_2.png");
-    song = loadSound('/assets/bg1.mp3');
+    //bgImage = loadImage("/image/main_2.png");
+    //song = loadSound('/assets/bg1.mp3');
 }
 
 function setup() {
     createCanvas(innerWidth, innerHeight);
-    song.play();
+    //song.play();
 }
   
 function draw() {
